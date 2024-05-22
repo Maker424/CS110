f1555a1a9f0a27c7933251d511efa91afce7db6c
Maker424 <jimmydelgado0812@gmail.com>
2024-05-22 16:29:36 -0700

    Have functional links to personal Github and LinkedIn profiles

--- ProfessionalWebsite/src/App.jsx
+++ ProfessionalWebsite/src/App.jsx
@@ -8,6 +8,8 @@ import Education from './components/Education'
 import Projects from './components/Projects'
 import Interests from './components/Interests'
 import Contact from './components/Contact'
+import GithubLogo from './assets/Github-Icon.png'
+import LinkedInLogo from './assets/LinkedIn-Icon.png'
 
 function App() {
   
@@ -49,6 +51,8 @@ function App() {
         </div>
         <div className="Contact-Section">
           <Contact></Contact>  
+          <a href="https://github.com/Maker424"> <img src={GithubLogo} alt="The Github Logo that leads to my personal Github Profile"/> </a>
+          <a href="https://www.linkedin.com/in/jimmy-delgado-hernandez-5889911ba"> <img src={LinkedInLogo} alt="The LinkedIn Logo That leads to LinkedIn homepage"/> </a>
         </div>
       </div>
     </>
