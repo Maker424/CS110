964098d1277491e2b99b8ad2f99922eff9ca1c44
Maker424 <jimmydelgado0812@gmail.com>
2024-05-22 23:14:03 -0700

    Finish Hero Section

--- ProfessionalWebsite/src/components/Hero.jsx
+++ ProfessionalWebsite/src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import UCR from '/src/assets/UCR-Logo.jpg'
+import UCRBook from '/src/assets/UCR-Book.png'
+import UCRBear from '/src/assets/UCR-Bear.png'
+
 const Hero = () => {    
   return (
     <div className="Hero-Body">
-        <h2>Hero Section</h2>
         <p>My name is Jimmy Delgado-Hernandez</p>
-        <p>Currently I am almost done pursuing my Bachelor's Degree at the University of California Riverside.</p>
+        <p>Currently I am almost complete in pursuing my Bachelor's Degree in Computer Science at the University of California Riverside.</p>
+        <img src={UCRBear} alt="UCR Logo with a Bear instead" id="UCRBear" />
+        <img src={UCR} alt="UCR Logo"/>
+        <img src={UCRBook} alt="UCR Logo with Book in the middle" id="UCRBook"/>
     </div>                
   )
 }
