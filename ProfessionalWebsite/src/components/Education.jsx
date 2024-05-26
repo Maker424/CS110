9551f40fe2f5e4eb3a588ab509870faf0f3c020b
Maker424 <jimmydelgado0812@gmail.com>
2024-05-26 02:40:13 -0700

    finished Education Section?

--- ProfessionalWebsite/src/components/Education.jsx
+++ ProfessionalWebsite/src/components/Education.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './Education.css'
 import Cplusplus from '/src/assets/C++.png'
 import SQALogo from '/src/assets/SQA.png'
+import Githublogo from '/src/assets/Github-Icon.png'
+import CSSLogo from '/src/assets/CSS-Logo.png'
+import SQLLogo from '/src/assets/SQL.png'
 
 const Education = () => {    
   return (
     <div className="Education-Body">
       <h2>Education</h2>
       <div className="Academic-History">
-        <h3>Academic History</h3>
+        <h3>Academic History: </h3>
         <ul>
           <li><b>B.S. Computer Science</b> <i>- University of California, Riverside (2024)</i></li>
           <li><b>High School Diploma</b> <i>- Synergy Quantum Academy (2016 - 2020)</i></li>
@@ -36,6 +39,9 @@ const Education = () => {
       </div>
       <img src={Cplusplus} alt="C++ logo" id="CplusplusID" />
       <a href="https://www.synergyquantumacademy.org/"> <img src={SQALogo} alt="My SQA highschool logo" id="SQAID"/> </a>
+      <img src={Githublogo} alt="Github Logo" id="GitID" />
+      <img src={CSSLogo} alt="CSS3 Logo" id="CSSID"/>
+      <img src={SQLLogo} alt="SQL Logo" id="SQLID"/>
 
       <div className="Highschool-coursework">
         <h3>Highschool Relevant Coursework: </h3>
@@ -48,6 +54,11 @@ const Education = () => {
         <ul>
           <li>CS180: <i>Introduction to software Engineering</i></li>
           <li>CS142: <i>Algorithm Engineering</i></li>
+          <ul>
+            <a href="https://www.cs.ucr.edu/~yihans/teaching/142/W24/index.html"> <li> <i>Course Homepage link</i></li> </a>
+            
+          </ul>
+          <li>CS100: <i>Software Construction</i></li>
         </ul>
       </div>
 
